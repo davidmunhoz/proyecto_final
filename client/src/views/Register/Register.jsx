cfee47fb7ea10106416de2f4950ce190fd6ce38d
davidmunhoz <davidjesusmunozmartos@gmail.com>
2023-07-05 17:35:15 +0200

    register update and delete context js

--- client/src/views/Register/Register.jsx
+++ client/src/views/Register/Register.jsx
@@ -1,23 +1,8 @@
-// import {useState} from "react"
-// import { Grid, TextField, Typography, Button } from '@mui/material';
-
+import RegisterFormik from "../../components/RegisterFormik/RegisterFormik";
 
 export default function Register(){
-// const [body, setBody] = useState({name:"", email:"", password:""});
-
-
-
-// function onSubmit(e){
-//     e.preventDefault()
-//     console.log(body)
-// }
-
-
-// function handleChange(e){
-
-// }
 
     return(
-<Register/>
+<RegisterFormik/>
     )
 }
